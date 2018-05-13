9fee82f762aa41fed3c37fee63196f16e90d8917
Manohar K <kodurumanohar931@gmail.com>
2018-05-13 12:12:57 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
+
+//Create a li element
+
+const li=document.createElement('li');
+//Add class
+li.className='collection-item';
+//create a text node and append to li
+
+li.appendChild(document.createTextNode(taskInput.value));
+
+//create a new link element i.e. delete icon
+const link=document.createElement('a');
+//Add class
+link.className='delete-item secondary-content';
+
+//Add icon html
+
+link.innerHTML='<i class="fa fa-remove"></i>';
+//Append li to ui
+li.appendChild(link);
+
+
+//Append li to ul
+taskList.appendChild(li);
+
+//clear input
+taskInput.value='';
+
+
+  e.preventDefault();
+}
